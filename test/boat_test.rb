e8edaecd7ab81e3561cf7a2bbe57a05f4b88e51c
Kevin McGrevey <niv3kmcg@gmail.com>
2020-01-03 16:58:47 -0700

    Iteration1 complete

--- test/boat_test.rb
+++ test/boat_test.rb
@@ -6,39 +6,27 @@ require './lib/renter'
 
 class BoatTest < Minitest::Test
 
-  def test_it_exists
-    kayak = Boat.new(:kayak, 20)
+  def setup
+    @kayak = Boat.new(:kayak, 20)
+  end
 
-    assert_instance_of Boat, kayak
+  def test_it_exists
+    assert_instance_of Boat, @kayak
+  end
+
+  def test_it_has_attributes
+    assert_equal :kayak, @kayak.type
+    assert_equal 20, @kayak.price_per_hour
+  end
+
+  def test_it_can_total_hours_rented
+    assert_equal 0, @kayak.hours_rented
+
+    @kayak.add_hour
+    @kayak.add_hour
+    @kayak.add_hour
+
+    assert_equal 3, @kayak.hours_rented
   end
 
 end
-# pry(main)> kayak = Boat.new(:kayak, 20)
-# # => #<Boat:0x00007fceac8f0480...>
-#
-# pry(main)> kayak.type
-# # => :kayak
-#
-# pry(main)> kayak.price_per_hour
-# # => 20
-#
-# pry(main)> kayak.hours_rented
-# # => 0
-#
-# pry(main)> kayak.add_hour
-#
-# pry(main)> kayak.add_hour
-#
-# pry(main)> kayak.add_hour
-#
-# pry(main)> kayak.hours_rented
-# # => 3
-#
-# pry(main)> renter = Renter.new("Patrick Star", "4242424242424242")
-# # => #<Renter:0x00007fb5ef98b118...>
-#
-# pry(main)> renter.name
-# # => "Patrick Star"
-#
-# pry(main)> renter.credit_card_number
-# # => "4242424242424242"
