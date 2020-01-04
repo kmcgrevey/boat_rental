699064eed10b2eff10aca72e7e91041fd027bb8c
Kevin McGrevey <niv3kmcg@gmail.com>
2020-01-03 17:23:35 -0700

    Complete Iteration2

--- test/dock_test.rb
+++ test/dock_test.rb
@@ -8,6 +8,11 @@ class DockTest < Minitest::Test
 
   def setup
     @dock = Dock.new("The Rowing Dock", 3)
+    @kayak_1 = Boat.new(:kayak, 20)
+    @kayak_2 = Boat.new(:kayak, 20)
+    @sup_1 = Boat.new(:standup_paddle_board, 15)
+    @patrick = Renter.new("Patrick Star", "4242424242424242")
+    @eugene = Renter.new("Eugene Crabs", "1313131313131313")
   end
 
   def test_it_exists
@@ -18,36 +23,14 @@ class DockTest < Minitest::Test
     assert_equal "The Rowing Dock", @dock.name
     assert_equal 3, @dock.max_rental_time
   end
+
+  def test_it_can_create_a_rental_log
+    @dock.rent(@kayak_1, @patrick)
+    expected = {@kayak_1 => @patrick}
+
+    assert_equal expected, @dock.rental_log
+    @dock.rent(@kayak_2, @patrick)
+    @dock.rent(@sup_1, @eugene)
+    @dock.rental_log
+  end
 end
-
-
-
-#
-# pry(main)> kayak_1 = Boat.new(:kayak, 20)
-# # => #<Boat:0x00007fdeedb3a528...>
-#
-# pry(main)> kayak_2 = Boat.new(:kayak, 20)
-# # => #<Boat:0x00007fdeedae1860...>
-#
-# pry(main)> sup_1 = Boat.new(:standup_paddle_board, 15)
-# # => #<Boat:0x00007fdeedaa8bc8...>
-#
-# pry(main)> patrick = Renter.new("Patrick Star", "4242424242424242")
-# # => #<Renter:0x00007fdeed0ab828...>
-#
-# pry(main)> eugene = Renter.new("Eugene Crabs", "1313131313131313")
-# # => #<Renter:0x00007fdeed8ce5c8...>
-#
-# pry(main)> dock.rent(kayak_1, patrick)
-#
-# pry(main)> dock.rent(kayak_2, patrick)
-#
-# pry(main)> dock.rent(sup_1, eugene)
-#
-# pry(main)> dock.rental_log
-# # =>
-# # {
-# #   #<Boat:0x00007fdeedb3a528...> => #<Renter:0x00007fdeed0ab828...>,
-# #   #<Boat:0x00007fdeedae1860...> => #<Renter:0x00007fdeed0ab828...>,
-# #   #<Boat:0x00007fdeedaa8bc8...> => #<Renter:0x00007fdeed8ce5c8...>
-# # }
